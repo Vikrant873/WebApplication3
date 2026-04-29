422678c7d8a77d59f0331aa87eb0be167c1be78d
Vikrant873 <83238827+Vikrant873@users.noreply.github.com>
2026-04-29 11:18:23 +0530

    Update WeatherForecastController.cs

--- WebApplication3/Controllers/WeatherForecastController.cs
+++ WebApplication3/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace WebApplication3.Controllers
       _logger = logger;
     }
 
-    
+    // This is get method for whether forcast
     // This is get method for whether forcast
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
